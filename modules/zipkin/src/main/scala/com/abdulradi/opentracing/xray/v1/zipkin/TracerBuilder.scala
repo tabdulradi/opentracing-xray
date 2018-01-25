cdadbd4bc887a8192677d594a3738a67bec60d79
Tamer Mohammed Abdul-Radi <tamerr@cakesolutions.net>
2018-01-25 10:46:38 +0000

    Test cases and few corner case fixes

--- modules/zipkin/src/main/scala/com/abdulradi/opentracing/xray/v1/zipkin/TracerBuilder.scala
+++ modules/zipkin/src/main/scala/com/abdulradi/opentracing/xray/v1/zipkin/TracerBuilder.scala
@@ -3,8 +3,7 @@ package com.abdulradi.opentracing.xray.v1.zipkin
 import java.lang.Boolean
 import java.util
 
-import scala.util.Random
-import cats.syntax.either._ // Needed to cross-compile to Scala 2.11
+import cats.syntax.either._
 import brave.Tracing
 import brave.opentracing.BraveTracer
 import brave.propagation.Propagation.{Getter, KeyFactory, Setter}
@@ -46,7 +45,7 @@ class XRayPropagationFactory() extends Propagation.Factory {
 
 class XRayPropagation[K](keyFactory: KeyFactory[K], underlying: Propagation[K]) extends Propagation[K] {
   override def injector[C](setter: Setter[C, K]): Injector[C] =
-    underlying.injector(setter)
+    new XRayInjector(underlying.injector[C] _, setter)
 
   override def keys(): util.List[K] =
     underlying.keys()
@@ -55,19 +54,31 @@ class XRayPropagation[K](keyFactory: KeyFactory[K], underlying: Propagation[K]) 
     new XRayExtractor(getter, keyFactory)
 }
 
+class ManglingSetter[C, K](underlying: Setter[C, K]) extends Setter[C, K] {
+  override def put(carrier: C, key: K, value: String): Unit = {
+    underlying.put(carrier, key, value.replaceFirst(";Parent=0000000000000000", ""))
+  }
+}
+
+class XRayInjector[C, K](underlying: Setter[C, K] => Injector[C], setter: Setter[C, K]) extends Injector[C] {
+  override def inject(ctx: TraceContext, carrier: C): Unit =
+    underlying(if (ctx.parentId == null || ctx.parentId == 0L) new ManglingSetter(setter) else setter)
+      .inject(ctx, carrier)
+}
+
 class XRayExtractor[C, K](getter: Getter[C, K], keyFactory: KeyFactory[K]) extends Extractor[C] {
   override def extract(carrier: C): TraceContextOrSamplingFlags = {
     val header =
       xray.tracing.Propagation.parseOrGenerate(keyName => Option(getter.get(carrier, keyFactory.create(keyName))))
 
     val (high, low) = (header.rootTraceId.originalRequestTime.value + header.rootTraceId.identifier.value).splitAt(16)
-    val parentId = header.parentSegmentId.map(p => java.lang.Long.parseLong(p.value, 16))
+    val parentId = header.parentSegmentId.map(p => java.lang.Long.parseUnsignedLong(p.value, 16))
 
     TraceContextOrSamplingFlags.create(
       TraceContext
         .newBuilder
-        .traceIdHigh(java.lang.Long.parseLong(high, 16))
-        .traceId(java.lang.Long.parseLong(low, 16))
+        .traceIdHigh(java.lang.Long.parseUnsignedLong(high, 16))
+        .traceId(java.lang.Long.parseUnsignedLong(low, 16))
         .spanId(parentId.getOrElse(0)) // 0 is a special dummy value, filtered out by SpanConverter
         .shared(false) // Spans are never shared across multiple hosts, simply because tracing header doesn't include spanId
         .sampled(header.samplingDecision.map(s => new Boolean(s)).orNull)
--- modules/zipkin/src/test/scala/com/abdulradi/opentracing/xray/v1/zipkin/HeaderExtractionTest.scala
+++ modules/zipkin/src/test/scala/com/abdulradi/opentracing/xray/v1/zipkin/HeaderExtractionTest.scala
@@ -1,121 +1,121 @@
 package com.abdulradi.opentracing.xray.v1.zipkin
 
 import scala.collection.JavaConverters._
+import scala.collection.mutable
 
+import brave.opentracing.BraveTracer
 import brave.sampler.Sampler
-import io.opentracing.propagation.{Format, TextMapExtractAdapter}
+import io.opentracing.SpanContext
+import io.opentracing.propagation.{Format, TextMapExtractAdapter, TextMapInjectAdapter}
 import org.scalatest._
 
 class HeaderExtractionTest extends FunSuite {
-  val codec = TracerBuilder("", "", 0, Sampler.ALWAYS_SAMPLE).build()
-  val nonSamplercodec = TracerBuilder("", "", 0, Sampler.NEVER_SAMPLE).build()
+  val tracer = TracerBuilder("", "", 0, Sampler.ALWAYS_SAMPLE).build()
 
-  def headers(pairs: (String, String)*) = new TextMapExtractAdapter(pairs.toMap.asJava)
-  def traceHeader(value: String) = headers("x-amzn-trace-id" -> value)
-  val flagNotSampled: Byte = 0
-  val flagSampled: Byte = 1
+  def headers(pairs: (String, String)*) =
+    new TextMapExtractAdapter(pairs.toMap.asJava)
 
+  def getHeader(tracer: BraveTracer, context: SpanContext): String = {
+    val carrier = new mutable.LinkedHashMap[String, String].asJava
+    tracer.inject(context, Format.Builtin.HTTP_HEADERS, new TextMapInjectAdapter(carrier))
+    carrier.get("x-amzn-trace-id")
+  }
 
-  test("Foo") {
-    val context = codec.extract(Format.Builtin.HTTP_HEADERS, traceHeader("Root=1-5a5cd12f-f08cdaba788bc0a0f88f72e6"))
-    println(context === null) // 7aa???
+  def assertHeader(input: String, expectedOutput: String) = {
+    val context = tracer.extract(Format.Builtin.HTTP_HEADERS, headers("x-amzn-trace-id" -> input))
+    assert(getHeader(tracer, context) === expectedOutput)
   }
 
   test("Empty headers means random generated headers, sampler decide on sampling field") {
-    val context = codec.extract(Format.Builtin.HTTP_HEADERS, headers())
-    assert(context === null)
+    val context = tracer.extract(Format.Builtin.HTTP_HEADERS, headers())
+    val header = getHeader(tracer, context)
+    assert(header.length === 50)
+    assert(header.startsWith("Root=1-") === true)
+    assert(header(15) === '-')
+    assert(header.endsWith(";Sampled=?") === true)
   }
-//
-//  test("No tracing header (non relevant headers) shouldn't affect extraction") {
-//    val context = codec.extract(Format.Builtin.HTTP_HEADERS, headers("foo" -> "bar")).unwrap()
-//    val header = TracingHeader.fromSpanContext(context).right.get
-//    assert(context.getTraceId === 0) // Dummy traceId, since XRay's won't fit a Long
-//    assert(header.parentSegmentId === None)
-//    assert(context.getFlags === flagSampled)
-//    assert(header.samplingDecision === Some(1))
-//  }
-//
-  test("Root is the minimum, sampler decides whether to sample or not") {
-    val context = codec.extract(Format.Builtin.HTTP_HEADERS, traceHeader("Root=1-5a5cd12f-f08cdaba788bc0a0f88f72e6"))
-    println(context)
-//    val header = TracingHeader.fromSpanContext(context).right.get
-//    assert(context.getTraceId === 0) // Dummy traceId, since XRay's won't fit a Long
-//    assert(header.rootTraceId.originalRequestTime.value === "5a5cd12f")
-//    assert(header.rootTraceId.identifier.value === "f08cdaba788bc0a0f88f72e6")
-//    assert(header.parentSegmentId === None)
-//    assert(header.samplingDecision === Some(1))
-//
-//    val context2 = nonSamplercodec.extract(Format.Builtin.HTTP_HEADERS, traceHeader("Root=1-5a5cd12f-f08cdaba788bc0a0f88f72e6")).unwrap()
-//    val header2 = TracingHeader.fromSpanContext(context2).right.get
-//    assert(context2.getFlags === flagNotSampled)
-//    assert(header2.samplingDecision === Some(0))
+
+  test("No tracing header (non relevant headers) shouldn't affect extraction") {
+    val context = tracer.extract(Format.Builtin.HTTP_HEADERS, headers("foo" -> "bar"))
+    val header = getHeader(tracer, context)
+    assert(header.length === 50)
+    assert(header.startsWith("Root=1-") === true)
+    assert(header(15) === '-')
+    assert(header.endsWith(";Sampled=?") === true)
   }
-//
-//  test("Lower case tracing header is still valid") {
-//    val context = codec.extract(Format.Builtin.HTTP_HEADERS, headers("x-amzn-trace-id" -> "Root=1-5a5cd12f-f08cdaba788bc0a0f88f72e6")).unwrap()
-//    val header = TracingHeader.fromSpanContext(context).right.get
-//    assert(header.rootTraceId.originalRequestTime.value === "5a5cd12f")
-//    assert(header.rootTraceId.identifier.value === "f08cdaba788bc0a0f88f72e6")
-//  }
-//
-//  test("Sampled should be obeyed") {
-//    val context1 = codecNoSampler.extract(traceHeader("Root=1-5759e988-bd862e3fe1be46a994272793;Sampled=1"))
-//    val header1 = TracingHeader.fromSpanContext(context1).right.get
-//    assert(header1.samplingDecision === Some(1))
-//    assert(header1.rootTraceId.originalRequestTime.value === "5759e988")
-//    assert(header1.rootTraceId.identifier.value === "bd862e3fe1be46a994272793")
-//
-//    val context2 = codecNoSampler.extract(traceHeader("Root=1-5759e988-bd862e3fe1be46a994272793;Sampled=0"))
-//    val header2 = TracingHeader.fromSpanContext(context2).right.get
-//    assert(header2.samplingDecision === Some(0))
-//    assert(header2.rootTraceId.originalRequestTime.value === "5759e988")
-//    assert(header2.rootTraceId.identifier.value === "bd862e3fe1be46a994272793")
-//  }
-//
-//  test("Parent should be parsed") {
-//    val context = codec.extract(Format.Builtin.HTTP_HEADERS, traceHeader("Root=1-5a5cd12f-f08cdaba788bc0a0f88f72e6;Parent=6a3dc251f20785d7")).unwrap()
-//    val header = TracingHeader.fromSpanContext(context).right.get
-//    assert(header.parentSegmentId.get.value === "6a3dc251f20785d7")
-//    assert(header.samplingDecision === Some(1)) // Test codec always samples if no Sampled header
-//    assert(header.rootTraceId.originalRequestTime.value === "5a5cd12f")
-//    assert(header.rootTraceId.identifier.value === "f08cdaba788bc0a0f88f72e6")
-//  }
-//
-//  test("Root, Parent, Sampled combo") {
-//    val context = codecNoSampler.extract(traceHeader("Root=1-5a5cd12f-f08cdaba788bc0a0f88f72e6;Parent=6a3dc251f20785d7;Sampled=1"))
-//    val header = TracingHeader.fromSpanContext(context).right.get
-//    assert(header.parentSegmentId.get.value === "6a3dc251f20785d7")
-//    assert(header.samplingDecision === Some(1))
-//    assert(header.rootTraceId.originalRequestTime.value === "5a5cd12f")
-//    assert(header.rootTraceId.identifier.value === "f08cdaba788bc0a0f88f72e6")
-//  }
-//
-//  test("Root, Parent, not Sampled combo") {
-//    val context = codecNoSampler.extract(traceHeader("Root=1-5a5cd12f-f08cdaba788bc0a0f88f72e6;Parent=6a3dc251f20785d7;Sampled=0"))
-//    val header = TracingHeader.fromSpanContext(context).right.get
-//    assert(header.parentSegmentId.get.value === "6a3dc251f20785d7")
-//    assert(header.samplingDecision === Some(0))
-//    assert(header.rootTraceId.originalRequestTime.value === "5a5cd12f")
-//    assert(header.rootTraceId.identifier.value === "f08cdaba788bc0a0f88f72e6")
-//  }
-//
-//  test("trailing simicolon shouldn't confuse the parser") {
-//    val context = codec.extract(Format.Builtin.HTTP_HEADERS, traceHeader("Root=1-5a5cd12f-f08cdaba788bc0a0f88f72e6;Parent=6a3dc251f20785d7;")).unwrap()
-//    val header = TracingHeader.fromSpanContext(context).right.get
-//    assert(context.getTraceId === 0) // Dummy traceId, since XRay's won't fit a Long
-//    assert(header.parentSegmentId.get.value === "6a3dc251f20785d7")
-//    assert(header.samplingDecision === Some(1))
-//    assert(header.rootTraceId.originalRequestTime.value === "5a5cd12f")
-//    assert(header.rootTraceId.identifier.value === "f08cdaba788bc0a0f88f72e6")
-//  }
-//
-//  test("unknown keys should ignored") {
-//    val context = codec.extract(Format.Builtin.HTTP_HEADERS, traceHeader("Root=1-5a5cd12f-f08cdaba788bc0a0f88f72e6;Parent=6a3dc251f20785d7;Foo=Bar")).unwrap()
-//    val header = TracingHeader.fromSpanContext(context).right.get
-//    assert(context.getTraceId === 0) // Dummy traceId, since XRay's won't fit a Long
-//    assert(header.parentSegmentId.get.value === "6a3dc251f20785d7")
-//    assert(header.samplingDecision === Some(1))
-//    assert(header.rootTraceId.originalRequestTime.value === "5a5cd12f")
-//    assert(header.rootTraceId.identifier.value === "f08cdaba788bc0a0f88f72e6")
-//  }
+
+  test("Root is the minimum") {
+    assertHeader(
+      "Root=1-5a5cd12f-f08cdaba788bc0a0f88f72e6",
+      "Root=1-5a5cd12f-f08cdaba788bc0a0f88f72e6;Sampled=?"
+    )
+  }
+
+  test("Max traceId should fit") {
+    assertHeader(
+      "Root=1-ffffffff-ffffffffffffffffffffffff",
+      "Root=1-ffffffff-ffffffffffffffffffffffff;Sampled=?"
+    )
+  }
+
+  test("Lower case tracing header is still valid") {
+    val context = tracer.extract(Format.Builtin.HTTP_HEADERS, headers("x-amzn-trace-id" -> "Root=1-5a5cd12f-f08cdaba788bc0a0f88f72e6"))
+    assert(getHeader(tracer, context) === "Root=1-5a5cd12f-f08cdaba788bc0a0f88f72e6;Sampled=?")
+  }
+
+  test("Sampled should be parsed") {
+    assertHeader(
+      "Root=1-5759e988-bd862e3fe1be46a994272793;Sampled=1",
+      "Root=1-5759e988-bd862e3fe1be46a994272793;Sampled=1"
+    )
+  }
+
+  test("Parent should be parsed") {
+    assertHeader(
+      "Root=1-5a5cd12f-f08cdaba788bc0a0f88f72e6;Parent=6a3dc251f20785d7",
+      "Root=1-5a5cd12f-f08cdaba788bc0a0f88f72e6;Parent=6a3dc251f20785d7;Sampled=?"
+    )
+  }
+
+  test("Max Parent should fit") {
+    assertHeader(
+      "Root=1-ffffffff-ffffffffffffffffffffffff;Parent=ffffffffffffffff;Sampled=1",
+      "Root=1-ffffffff-ffffffffffffffffffffffff;Parent=ffffffffffffffff;Sampled=1"
+    )
+  }
+
+  test("Root, Parent, Sampled combo") {
+    assertHeader(
+      "Root=1-5a5cd12f-f08cdaba788bc0a0f88f72e6;Parent=6a3dc251f20785d7;Sampled=1",
+      "Root=1-5a5cd12f-f08cdaba788bc0a0f88f72e6;Parent=6a3dc251f20785d7;Sampled=1"
+    )
+  }
+
+  test("Root, Parent, not Sampled combo") {
+    assertHeader(
+      "Root=1-5a5cd12f-f08cdaba788bc0a0f88f72e6;Parent=6a3dc251f20785d7;Sampled=0",
+      "Root=1-5a5cd12f-f08cdaba788bc0a0f88f72e6;Parent=6a3dc251f20785d7;Sampled=0"
+    )
+  }
+
+  test("Root, Parent, might be Sampled combo") {
+    assertHeader(
+      "Root=1-5a5cd12f-f08cdaba788bc0a0f88f72e6;Parent=6a3dc251f20785d7;Sampled=?",
+      "Root=1-5a5cd12f-f08cdaba788bc0a0f88f72e6;Parent=6a3dc251f20785d7;Sampled=?"
+    )
+  }
+
+  test("trailing simicolon shouldn't confuse the parser") {
+    assertHeader(
+      "Root=1-5a5cd12f-f08cdaba788bc0a0f88f72e6;Parent=6a3dc251f20785d7;Sampled=0;",
+      "Root=1-5a5cd12f-f08cdaba788bc0a0f88f72e6;Parent=6a3dc251f20785d7;Sampled=0"
+    )
+  }
+
+  test("unknown keys should ignored for now") { // TODO: We should actually propagate them as is
+    assertHeader(
+      "Root=1-5a5cd12f-f08cdaba788bc0a0f88f72e6;Parent=6a3dc251f20785d7;Foo=Bar",
+      "Root=1-5a5cd12f-f08cdaba788bc0a0f88f72e6;Parent=6a3dc251f20785d7;Sampled=?"
+    )
+  }
 }
